d9b7062f1e0b463935d2789ced3eda7d9de805dd
Gopal <166762157+Gopal7715@users.noreply.github.com>
2025-05-07 16:32:19 +0530

    Time: 0 ms (100%), Space: 9.2 MB (55.09%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> count(26,0);
+
+        for(char ch : s) {
+            count[ch - 'a']++;
+        }
+
+        for(char ch : t) {
+
+            count[ch - 'a']--;
+            if(count[ch - 'a'] < 0) return ch;
+        }
+
+        return ' ';
+    }
+};
